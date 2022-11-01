5abcd46816965b488f8d9e99c38c7425ba484606
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-01 10:05:13 -0300

    Resolução

--- Atividade05/main.c
+++ Atividade05/main.c
@@ -1,86 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 #include "pilha.h"
 
-char *posfixa(char *e) {
-    static char s[256];
-    int j = 0;
-    Pilha P = pilha(256);
+int main() {
+    char e[32];
+    char *posfix;
+    int y;
+    int x;
+    int n;
+    Pilha *p = pilha(32);
 
-    for (int i = 0; e[i]; i++) {
+    printf("Infixa: ");
+    fgets(e,31,stdin);
+    posfix = posfixa(e);
+    printf("Posfixa: %s\n", posfix);
 
-        if (isdigit(e[i])) {
-            s[j++] = e[i];
+// ((2*3)+(8/4))
+// ((2+3)+(8+4))
+    for (int i = 0; posfix[i]; i++){
+        if (isdigit(posfix[i])){
+            push(posfix[i] - '0',p);
+        } else {
+            y = pop(p);
+            x = pop(p);
+            switch(posfix[i]){
+                case '+':
+                    push(y+x,p);
+                    break;
+                case '-':
+                    push(y-x,p);
+                    break;
+                case '*':
+                    push(y*x,p);
+                    break;
+                case '/':
+                    if (x > y){
+                        push(x/y,p);
+                    } else {
+                        push(y/x,p);
+                    }
+                    break;
+            }
         }
-
-        if (strchr("+*-/", e[i])) {
-            push(e[i], P);
-        }
-
-        if (e[i] == ')') {
-            s[j++] = pop(P);
-        }
-
     }
 
-    s[j] = '\0';
-    destroy(&P);
-
-    return s;
-}
-
-int main()
-{
-    Pilha P = pilha(256);
-
-    int num1, num1a, num2, num2a;
-    float resultado;
-    char e[256];
-
-    printf("Infixa? ");
-    gets(e);
-
-    char *exp_pos = posfixa(e);
-    printf("Forma da Expressao Posfixa = %s\n", exp_pos);
-
-    for(int i = 0; exp_pos[i]; i++) {
-
-        if(isdigit(exp_pos[i])) {
-            num1 = exp_pos[i];
-            num1a = num1 - '0';
-
-            push(num1a, P);
-        }
-
-        if (strchr("+*-/", exp_pos[i])) {
-            num1 = pop(P);
-            num2 = pop(P);
-
-            if(exp_pos[i] == '+') {
-                resultado = num1 + num2;
-                push(resultado, P);
-            }
-
-            if(exp_pos[i] == '-') {
-                resultado = num1 - num2;
-                push(resultado, P);
-            }
-
-            if(exp_pos[i] == '*') {
-                resultado = num1 * num2;
-                push(resultado, P);
-            }
-
-            if(exp_pos[i] == '/') {
-                resultado = num2 / num1;
-                push(resultado, P);
-            }
-        }
-
-    }
-
-   printf("Resultado da Expressao = %d", getTop(P));
+    printf("Resultado: %s",desempilha(p));
 
     return 0;
 }
