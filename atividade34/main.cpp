146558779cdf2d23c0477ce93c0cb40e57a0724a
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-17 16:53:51 -0300

    Resolução

--- atividade34/main.cpp
+++ atividade34/main.cpp
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+
+using namespace std;
+
+typedef struct aluno {
+	int matricula;
+	char nome[20];
+}Aluno;
+
+
+
+void ord_insercao(Aluno v[],int tam);
+void selection_sort(Aluno v[],int n);
+void bubble_sort(Aluno v[],int n);
+
+
+Aluno myarray[20];
+	
+main(){
+	 //1:  A. leia os valores e adicione no vetor de dados
+	 // ordene pelo campo matricula
+	 	//   B. Ordene por:
+		//   c. Insercao
+	// mostre o resultado ordenado
+	
+	// 2: A. leia os valores e adicione no vetor de dados
+	// ordene pelo campo nome 
+		//  B. Ordene por:
+	//  c. selection_sort
+	// mostre o resultado ordenado
+	
+	// 3: A. leia os valores e adicione no vetor de dados
+	// ordene pelo campo matricula
+	//  B. Ordene por:
+	//  c. bubble_sort
+	// mostre o resultado ordenado
+	
+	
+}
+
+void ord_insercao(Aluno v[],int tam){
+	}
+
 
