b0544670fb71f3febff6a935659d30429495cf33
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-22 16:22:52 -0300

    Resolução

--- atividade35/main.cpp
+++ atividade35/main.cpp
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<iostream>
-using namespace std;
-void Quick(int vetor[10], int inicio, int fim);
+#define troca(a, b) {int x = a; a = b; b = x; }
 
+using namespace std;
+void quickSort(int vetor[10], int inicio, int fim);
 int main(){
    int vetor[6] = {7, 9, 4, 3,6,1};
    int i;   
-   Quick(vetor, 0, 5);
+   quickSort(vetor, 0, 5);
    printf("\n2.Vetor ordenado:\n");
    for(i = 0; i <= 5; i++){
       printf("%d ", vetor[i]);
@@ -14,8 +15,22 @@ int main(){
    printf("\n");   
 }
 
-void Quick(int vetor[10], int inicio, int fim){
-    
+int particiona (int v[], int inicio, int fim) {
+    int x = v[inicio];
+    inicio--;
+    fim++;
+    while(inicio<fim){
+        do fim--; while(v[fim]>x);
+        do inicio++; while(v[inicio]<x);
+        if (inicio < fim) troca(v[inicio], v[fim]);
+    }
+    return fim;
 }
 
+void quickSort(int v[], int inicio, int fim){
+    if(inicio >= fim) return;
+    int m = particiona(v, inicio, fim);
+    quickSort(v, inicio, m);
+    quickSort(v, m+1, fim);
+}
 
